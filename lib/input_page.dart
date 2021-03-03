d57b4b6dde74bcf0eadd0cb845291b25dd5672ca
RahmsMusti <42654569+RahmsMusti@users.noreply.github.com>
2021-03-03 23:50:41 +0000

    alternating gender buttons working
    about to use ternary operator

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -6,7 +6,10 @@ import 'package:start_keto/reusable_card.dart';
 
 const bottomContainerHeight = 60.0;
 const activeCardColour = Color(0xFF1D1E33);
+const inactiveCardColour = Color(0xFF111328);
 const bottomContainerColour = Color(0xFFEB1555);
+
+enum Gender { male, female }
 
 class InputPage extends StatefulWidget {
   @override
@@ -14,6 +17,29 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  Color maleCardColour = inactiveCardColour;
+  Color femaleCardColour = inactiveCardColour;
+
+  // void updateColour(Gender gender) {
+  //   if (gender == Gender.male) {
+  //     if (maleCardColour == inactiveCardColour) {
+  //       maleCardColour = activeCardColour;
+  //       femaleCardColour = inactiveCardColour;
+  //     } else {
+  //       maleCardColour = inactiveCardColour;
+  //     }
+  //   } else {
+  //     if (gender == Gender.female) {
+  //       if (femaleCardColour == inactiveCardColour) {
+  //         femaleCardColour = activeCardColour;
+  //         maleCardColour = inactiveCardColour;
+  //       } else {
+  //         femaleCardColour = inactiveCardColour;
+  //       }
+  //     }
+  //   }
+  // }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,23 +55,38 @@ class _InputPageState extends State<InputPage> {
                   child: Row(
                     children: [
                       Expanded(
-                        child: ReusableCard(
-                          cardChild: IconContent(
-                            contentName: 'MALE',
-                            icon: FontAwesomeIcons.mars,
+                        child: GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              updateColour(Gender.male);
+                            });
+                            ;
+                          },
+                          child: ReusableCard(
+                            cardChild: IconContent(
+                              contentName: 'MALE',
+                              icon: FontAwesomeIcons.mars,
+                            ),
+                            colour: maleCardColour,
+                            height: 170.0,
                           ),
-                          colour: activeCardColour,
-                          height: 170.0,
                         ),
                       ),
                       Expanded(
-                        child: ReusableCard(
-                          cardChild: IconContent(
-                            contentName: 'FEMALE',
-                            icon: FontAwesomeIcons.venus,
+                        child: GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              updateColour(Gender.female);
+                            });
+                          },
+                          child: ReusableCard(
+                            cardChild: IconContent(
+                              contentName: 'FEMALE',
+                              icon: FontAwesomeIcons.venus,
+                            ),
+                            colour: femaleCardColour,
+                            height: 170.0,
                           ),
-                          colour: activeCardColour,
-                          height: 170.0,
                         ),
                       ),
                     ],
